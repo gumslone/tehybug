c20c6099411d4da33dde3298fc17610a88566c39
gumslone <gumslone@i.ua>
2018-05-23 15:14:12 +0300

    changed sensor variable to bug_key

--- php example/track.php
+++ php example/track.php
@@ -6,14 +6,14 @@
 	create a data folder in the same path as this script and make it writable, chmod 0777
 */
 echo 'OK';
-if(!empty($_REQUEST['sensor']))
+if(!empty($_REQUEST['bug_key']))
 {
 
-	if(!file_exists('./data/'.$_REQUEST['sensor'].''))
+	if(!file_exists('./data/'.$_REQUEST['bug_key'].''))
 	{
-		mkdir('./data/'.$_REQUEST['sensor'].'');
+		mkdir('./data/'.$_REQUEST['bug_key'].'');
 	}
-	$sensor_dir = $_REQUEST['sensor'].'/';
+	$sensor_dir = $_REQUEST['bug_key'].'/';
 	
 	
 	file_put_contents('./data/'.$sensor_dir.date("Y-m-d").'_sensor_data.txt', 'time='.time().'&'.$_SERVER['QUERY_STRING']."\n", FILE_APPEND | LOCK_EX);
